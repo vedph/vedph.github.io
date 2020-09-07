1545e4b5e63bcc415968a42bed149e03a231fc82
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2020-09-07 17:22:28 +0200

    jekill - seminars page - seminars info added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(window).on("scroll load resize", () => {
-  //The point where the navbar changes in px
-  let startY = $('.navbar').height() * 2;
-
-  if($(window).scrollTop() > startY){
+  if($(window).scrollTop()) {
     $('.navbar').addClass("top-nav-collapse");
   }else{
     $('.navbar').removeClass("top-nav-collapse");
