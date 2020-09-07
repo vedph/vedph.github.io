c0f6b7c622ba24cd5a5cfb3be70ea799a800db92
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2020-09-07 16:58:44 +0200

    Let's use jekyll + carousel

---
+++ js/seminars.js
@@ -0,0 +1,44 @@
+let seminars = [
+  {
+    date: "30 September 2020, h 17:00",
+    datelimit: 20200930,
+    who: "Diego Mantoan",
+    from: "VeDPH, Ca’ Foscari University of Venice",
+    title: "We Need a Database! Digitization Practices in Contemporary Art Ventures and Emergent Trends",
+  },
+  {
+    date: "14 October 2020, h 11:00",
+    datelimit: 20201014,
+    who: "Tanya Evans",
+    from: "Macquarie University, Sydney",
+    title: "History, Heritage and Environmental Change in a Deindustrialised Landscape",
+  },
+  {
+    date: "28 October 2020, h 17:00",
+    datelimit: 20201028,
+    who: "Joris van Zundert",
+    from: "Huygens Institute for the History of the Netherlands",
+    title: "The Bestseller Code: Predicaments of Predicting High Value Fiction",
+  },
+  {
+    date: "11 November 2020, h 17:00",
+    datelimit: 20201111,
+    who: "Sarah May",
+    from: "Swansea University",
+    title: "Infrastructure, Skills, Voice: Digital Participation Beyond Access in Post-Industrial Heritage",
+  },
+  {
+    date: "25 November 2020, h 17:00",
+    datelimit: 20201125,
+    who: "Samuel J. Huskey",
+    from: "University of Oklahoma",
+    title: "What is ‘Digital Latin’? An Overview of the Digital Latin Library",
+  },
+  {
+    date: "9 December 2020, h 17:00",
+    datelimit: 20201209,
+    who: "Abhishek Dutta & Matilde Malaspina",
+    from: "Oxford University",
+    title: "1516: A Visually Searchable Database of Early-Printed Illustration from the 15th and 16th centuries",
+  }
+];
