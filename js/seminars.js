3d572ff8c50f6c347be2f0bf07ce1ea02c9295a2
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2020-09-10 18:26:53 +0200

    Changes on navbar and seminarsseries

--- js/seminars.js
+++ js/seminars.js
@@ -5,6 +5,7 @@ let seminars = [
     who: "Diego Mantoan",
     from: "VeDPH, Ca’ Foscari University of Venice",
     title: "We Need a Database! Digitization Practices in Contemporary Art Ventures and Emergent Trends",
+    abstract: "ABSTRACT: What do an art dealer, a private collection, an artist studio or an estate have in common in the contemporary art sector? Usually –besides the same names of high calibre personalities in the address book– they all believe they need a database: and they need it now! They don't know exactly what they want it for and what kind of database they really need, but for sure it must be both tailor made and as good as their friend's or competitor's one. Indeed, in the last decade there was a true “database run” in the field of small and medium sized art ventures to better organise their internal workflow and to keep up with big museums. Diego Mantoan builds upon his direct expertise in planning and delivering digitisation projects for contemporary art institutes, in order to focus on theoretical issues and practical problems that arise when developing digital art archives in a sector that needs databases not just for research purposes, but rather to tackle managerial tasks as well as to enhance one’s art historical position and to strengthen market reputation.The seminar will be held on Zoom at this link",
   },
   {
     date: "14 October 2020, h 11:00",
